f8318eea213797de5f6c8a7888b1574ae814a70b
Will Decker <will.decker@gatech.edu>
2025-05-22 13:09:47 -0400

    matrix checks

--- lintransforms/utils.py
+++ lintransforms/utils.py
@@ -23,3 +23,15 @@ def correlate(x1:np.ndarray, x2:np.ndarray, r:float, axis:int=0):
     x1 = np.expand_dims(x1, axis=axis)
     x2 = np.expand_dims(x2, axis=axis)
     return r * x1 + np.sqrt(1 - r**2) * x2
+
+issymmetric = lambda x: np.all(np.isclose(x, x.T, atol=1e-8))
+issymmetric.__doc__ = "Check if a matrix is symmetric."
+
+issquare = lambda x: x.ndim == 2 and x.shape[0] == x.shape[1]
+issquare.__doc__ = "Check if a matrix is square."
+
+isidempotent = lambda x: np.all(np.isclose(x @ x, x, atol=1e-8)) and issquare(x)
+isidempotent.__doc__ = "Check if a matrix is idempotent."
+
+isfullrank = lambda x: np.linalg.matrix_rank(x) == min(x.shape)
+isfullrank.__doc__ = "Check if a matrix is full rank."
